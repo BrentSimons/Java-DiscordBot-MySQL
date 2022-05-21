d380b774a6a0e4a002dfaa5c10e100d2d68902ef
Br <r0878913@student.thomasmore.be>
2022-05-21 16:05:18 +0200

    upload sql file

---
+++ main_db_setup.sql
@@ -0,0 +1,32 @@
+drop table users;
+drop schema discord;
+
+create schema discord;
+use discord;
+
+create table Users
+(
+    discordID         bigint primary key,
+    discordUsername   varchar(128) not null,
+    totalSentMessages int          not null,
+    serverLevel       int          not null,
+    serverXp          int          not null
+);
+
+# --------------------------------------------------------------
+
+INSERT INTO `discord`.`users`
+(discordID,
+ discordUsername,
+ totalSentMessages,
+ serverLevel,
+ `serverXp`)
+VALUES (259751295856697856, "Yarin", 10, 1);
+
+INSERT INTO `discord`.`users`
+(`discordID`,
+ `discordUsername`,
+ `totalSentMessages`,
+ `serverLevel`,
+ `serverXp`)
+VALUES (269691295856697856, "Brent", 35, 3);
